dd5cd5c82e41b78012ea2269efa470b7efa724bd
yandeng110 <2963244248@qq.com>
2019-08-29 22:43:52 +0800

    登陆页面静态页完成

---
+++ public/back/js/login.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('#form').bootstrapValidator({
+        fields:{
+            username:{
+                validators:{
+                    notEmpty:{
+                        message:'用户名不能为空'
+                    },
+                    stringLength:{
+                        min:2,
+                        max:6,
+                        message:'用户名长度必须在 2-6位'
+                    }
+                }
+            },
+            password:{
+                validators:{
+                    notEmpty:{
+                        message:'密码不能为空'
+                    },
+                    stringLength:{
+                        min:6,
+                        max:12,
+                        message:'密码长度为12位'
+                    }
+                }
+            }
+        }
+    });
+})
