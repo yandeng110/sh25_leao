2eac277958e339fab43862526a7c833492606a6a
yandeng110 <2963244248@qq.com>
2019-08-31 00:17:48 +0800

    ajax登陆完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,10 @@
 $(function(){
     $('#form').bootstrapValidator({
+        　feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
         fields:{
             username:{
                 validators:{
@@ -10,6 +15,9 @@ $(function(){
                         min:2,
                         max:6,
                         message:'用户名长度必须在 2-6位'
+                    },
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
@@ -22,9 +30,39 @@ $(function(){
                         min:6,
                         max:12,
                         message:'密码长度为12位'
+                    },
+                    callback:{
+                        message:'密码错误'
                     }
                 }
             }
         }
     });
+
+    $('#form').on('success.form.bv',function(e){
+        e.preventDefault();
+        $.ajax({
+            type:'post',
+            url:' /employee/employeeLogin',
+            data:$('#form').serialize(),
+            dataType:'json',
+            success:function( info ){
+                if( info.success ){
+                    location.href = 'index.html'
+                }
+                if(info.error === 1000){
+                    // alert('当前用户名不存在')
+                    $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
+                }
+                if(info.error === 1001){
+                    $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
+                }
+            }
+        })
+    })
+
+    $('[type="reset"]').click(function(){
+
+        $('#form').data('bootstrapValidator').resetForm();
+    })
 })
